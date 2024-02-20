ce304e71f0a23f817283f2d6f4122f606ef7c3de
ashu <ashitoshpatil8981@gmail.com>
2024-02-20 21:24:54 +0530

    new

---
+++ loops/each.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "my_instance" {
+    for_each = toset(var.ami_id)
+    ami = each.value
+    instance_type = "t2.micro"
+    key_name = "MUMBAI"
+    tags = {
+        Name = "for_each"
+    }
+}
+
+variable "ami_id" {
+   default = ["ami-0e670eb768a5fc3d4","ami-05a5bb48beb785bf1","ami-059aa0510020f477b", "ami-03f4878755434977f"] 
+}
+
